7a9ea4ba37c91205f2539dc22c26e1a82665cef9
adamcesco <acescobedo@smu.edu>
2024-04-02 01:36:13 -0500

    folder structuring

--- src/popup/App.jsx
+++ src/popup/App.jsx
@@ -1,5 +1,5 @@
 /* global chrome */
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* global __dirname */
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        popup: path.resolve(__dirname, 'src/popup/index.html'),
+        background: path.resolve(__dirname, 'src/background/index.js'),
+        content: path.resolve(__dirname, 'src/content/index.js'),
+        // options: path.resolve(__dirname, 'src/options/index.html'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+      },
+    },
+  },
   plugins: [react()],
-})
+});
